61459331fedd129d7f48ef2c924e364c392187fe
Lex Toumbourou <lextoumbourou@gmail.com>
2013-02-10 23:46:33 +1100

    Added support for configuring disks

--- linode_create.py
+++ linode_create.py
@@ -2,7 +2,8 @@
 import sys
 import warnings
 
-# I have to catch the warning here because the linode api has a Runtime Warning
+# I have to catch the warning here because the Linode API module has a Runtime Warning,
+# this will need to be fixed before I'd recommend using this module since root passwords are sent
 with warnings.catch_warnings():
     warnings.filterwarnings("ignore", category=RuntimeWarning)
     try:
@@ -12,9 +13,9 @@ with warnings.catch_warnings():
         sys.exit(1)
 
 
-def host_exists(label, lin):
+def get_host_id(label, lin):
     """
-    Return True if specified host is already created or False
+    Return LINODEID if specified host is already created
 
     args:
         label - string - hostname
@@ -26,8 +27,9 @@ def host_exists(label, lin):
     # See if the hostname is already in the domain list, if not, we'll create it 
     for host in lin.linode_list():
         if host['LABEL'] == label:
-            return True
-    return False
+            return host['LINODEID']
+
+    return None
 
 
 def get_plan_id(plan, lin):
@@ -48,10 +50,7 @@ def get_plan_id(plan, lin):
         pass
 
     for p in lin.avail_linodeplans():
-        if p['PLANID'] == plan:
-            return p['PLANID']
-
-        if p['LABEL'] == plan:
+        if p['PLANID'] == plan or p['LABEL'] == plan:
             return p['PLANID']
 
     return None
@@ -62,17 +61,15 @@ def get_datacenter_id(datacenter, lin):
     Return datacenter id if valid
 
     args:
-        datacenter - string or int - name of datacenter
+        datacenter - string or int - name of datacenter or id
         lin - linode.api.Api object - connected Api class
     
     returns:
         int or None
     """
     # if it's a number, we'll ensure it's an int
-    try:
+    if datacenter.isdigit():
         datacenter = int(datacenter)
-    except ValueError:
-        pass
 
     for dc in lin.avail_datacenters():
         if type(datacenter) is int:
@@ -80,11 +77,73 @@ def get_datacenter_id(datacenter, lin):
                 return dc['DATACENTERID']
         # If it's not an int, I'll search the datacenter string for the 
         # specified datacenter, allowing for 'Tokyo' to match 'Tokyo, JP' etc
+        # I don't want to do string search everytime if the user gives an int
         else:
             if datacenter in dc['LOCATION']:
                 return dc['DATACENTERID']
 
     return None
+
+
+def get_distribution_id(distribution, lin):
+    """
+    Return distribution id if valid
+
+    args:
+        distribution - string or int - name of distribution or id
+        lin - linode.api.Api object - connected Api class
+
+    returns:
+        int or None
+    """
+    # if it's a number, we'll ensure it's an int
+    if distribution.isdigit():
+        distribution = int(distribution)
+
+    for d in lin.avail_distributions():
+        if d['DISTRIBUTIONID'] == distribution or d['LABEL'] == distribution:
+            return d['DISTRIBUTIONID']
+
+    return None
+
+def linode_is_booted(linode_id, lin):
+    """
+    Return True if Linode is booted
+
+    args:
+        linode_id - int - linode identifier
+        lin - linode.api.Api object - connected Api class
+
+    returns:
+        bool
+    """
+    data = lin.linode_list(LinodeID=linode_id)
+    if data and data[0]['STATUS'] == 1:
+        return True
+
+    return False
+
+def wait_for_job(job_id, timeout, lin):
+    """
+    Wait until a job is complete up until mandatory timeout value
+
+    args:
+        job_id - int - job identifier
+        timeout - int - time in seconds to wait for job to complete
+        lin - linode.api.Api object - connected Api class
+
+    returns:
+        bool
+    """
+    count = 0
+    while count <= timeout:
+        job = lin.linode_job_list(LinodeID=linode_id, JobID=res['JOBID'])
+        if job['HOST_SUCCESS'] == 1:
+            return True   
+        time.sleep(2)
+        count += 2
+
+    return False
 
 
 def main():
@@ -94,12 +153,31 @@ def main():
             datacenter = dict(required=False),
             api_key = dict(required=True),
             plan = dict(required=True),
+            payment_term = dict(required=True),
+            distribution = dict(required=True),
+            root_password = dict(required=True),
+            disk_size = dict(required=True),
+            state = dict(required=False, default='present'),
+            display_group = dict(required=False),
+            disk_label = dict(required=False, default='ansible_disk'),
+            swap_size = dict(required=False, default=256),
+            root_ssh_key = dict(required=False)
+            wait = dict(choices=BOOLEANS, default=False),
         )
     )
     label = module.params.get('name')
     datacenter = module.params.get('datacenter')
     api_key = module.params.get('api_key')
     plan = module.params.get('plan')
+    payment_term = module.params.get('payment_term')
+    distribution = module.params.get('distribution')
+    root_password = module.params.get('root_password')
+    disk_size = module.params.get('disk_size')
+    state = module.params.get('state')
+    display_group = module.params.get('display_group')
+    swap_size = module.params.get('swap_size')
+    root_ssh_key = module.params.get('root_ssh_key')
+    wait = module.params.get('wait')
 
     # Allow linode enviroment variables to be used if Ansible vars aren't set
     if not api_key and 'LINODE_API_KEY' in os.environ:
@@ -123,18 +201,76 @@ def main():
             msg = 'Plan %s not found' % plan)
 
     # Check that datacenter is valid, if so return the id
-    datacenter_id = get_datacenter_id(datacenter, lin)
-    if not datacenter_id:
+    dc_id = get_datacenter_id(datacenter, lin)
+    if not dc_id:
         module.fail_json(
             msg = 'Datacenter %s not found' % datacenter)
 
-    # Check that the payment term is valid, return the id if so
+    # Get distribution id if it's valid
+    dist_id = get_distribution_id(distribution, lin)
+    if not dist_id:
+        module.fail_json(
+            msg = 'Distribution %s not found' % distribution)
+
+    # Check that the payment term is valid
+    valid_payment_terms = [1, 12, 24]
+    payment_term = int(payment_term)
+    if payment_term not in valid_payment_terms:
+        module.fail_json(
+            msg = 'Payment term %s not valid' % datacenter)
+
+    # Get or create requested host
+    linode_id = get_host_id(label, lin)
+    if not linode_id:
+        try:
+            res = lin.linode_create(
+                DatacenterID=dc_id, PlanID=plan_id, PaymentTerm=payment_term)
+        except lin.api.Api as e: 
+            module.fail_json(
+                msg = 'Unable to create Linode. %s: %s' % (
+                    e.values[0]['ERRORCODE'], e.values[0]['ERRORMESSAGE']))
+
+        linode_id = res['LinodeID']
+
+        # Update details to ensure we can find it again
+        lin.linode_update(
+            LinodeID=linode_id, label=label, lpm_displayGroup=display_group)
+
+    # Check if we have a configuration profile, if not, create one from distribution
+    # if there's no configuration profile, then nothing will happen.
+    if not lin.linode_disk_list(LinodeID=linode_id):
+        # Create the Linode from distribution
+        kwargs = {'LinodeID': linode_id, 'DistributionID': dist_id, 
+                  'rootPass': root_password, 'Size': disk_size, 'label': 'Root Partition'}
+        if root_ssh_key:
+            kwargs['rootSSHKey'] = root_ssh_key
+        res = lin.linode_disk_createfromdistribution(**kwargs)
+
+        # Wait for the disk to be created, if requested
+        if wait:
+            wait_for_job(res['JOBID'])
+            
+        # Create the swap disk
+        kwargs = {'LinodeID': linode_id, 'Label': 'Swap Partition', 
+                  'Type': 'swap', 'Size': swap_size}
+        res = lin.linode_disk_create(**kwargs)
+
+        # Wait for the disk to be created, if requested
+        if wait:
+            wait_for_job(res['JOBID'])
 
 
+    # Update details
+    lin.linode_update(
+        LinodeID=linode_id, label=label, lpm_displayGroup=display_group)
 
-    if not host_exists(label, lin):
-        host_create(label, plan, datacenter, lin)
-       
+    # Check boot status
+    if state == 'booted' and not linode_is_booted(linode_id, lin):
+        res = lin.boot(LinodeID=linode_id)
+
+        # Wait for boot if requested
+        if wait:
+            wait_for_job(res['JOBID'])
 
 # this is magic, see lib/ansible/module_common.py
 #<<INCLUDE_ANSIBLE_MODULE_COMMON>>
