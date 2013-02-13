1f01abb6b395808a8473d6cbca6e4646c78fce13
Lex Toumbourou <lextoumbourou@gmail.com>
2013-02-13 23:55:03 +1100

    Added tear down command

--- test_scripts.py
+++ test_scripts.py
@@ -3,14 +3,25 @@ import subprocess
 
 import private
 
-current_dir = '.'
+test_module = 'ansible/hacking/test-module'
 
-# Start off by creating the host, or modify it if it exists
-command = ("{0}/ansible/hacking/test-module -m {1}/linode_create.py"
-           " -a 'name=\"{2}\" api_key=\"{3}\" plan=\"{4}\" datacenter=\"{5}\""
-           " payment_term=\"{6}\" state=\"present\" disk_size=\"{7}\""
-           " root_password=\"{8}\" distribution=\"{9}\" swap_size=256 wait=true'").format(
-                current_dir, current_dir, private.LABEL, private.API_KEY, 
+# ===============================================================
+# Tear down server if it exists
+# ===============================================================
+command = ("{0} -m ./linode_create.py -a "
+           "'name=\"{1}\" api_key=\"{2}\" state=\"absent\"'").format(
+                   test_module, private.LABEL, private.API_KEY)
+
+subprocess.call(command, shell=True)
+
+# ===============================================================
+# Create server
+# ===============================================================
+command = ("{0} -m ./linode_create.py"
+           " -a 'name=\"{1}\" api_key=\"{2}\" plan=\"{3}\" datacenter=\"{4}\""
+           " payment_term=\"{5}\" state=\"present\" disk_size=\"{6}\""
+           " root_password=\"{7}\" distribution=\"{8}\" swap_size=256 wait=true'").format(
+                test_module, private.LABEL, private.API_KEY, 
                 private.PLAN, private.DATACENTER, private.PAYMENT_TERM, private.DISK_SIZE,
                 private.ROOT_PASS, private.DISTRIBUTION) 
 
