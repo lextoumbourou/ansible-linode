4790872335dc4136c7a2e697991165eed269ca9b
Lex Toumbourou <lextoumbourou@gmail.com>
2013-02-22 23:52:06 +1100

    Changed test scripts path

--- test_scripts.py
+++ test_scripts.py
@@ -8,16 +8,29 @@ test_module = 'ansible/hacking/test-module'
 # ===============================================================
 # Tear down server if it exists
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py -a "
+command = ("{0} -m ./library/linode_manager -a "
            "'name=\"{1}\" api_key=\"{2}\" state=\"absent\"'").format(
-                   test_module, private.LABEL, private.API_KEY)
+               test_module, private.LABEL, private.API_KEY)
 
 subprocess.call(command, shell=True)
 
 # ===============================================================
 # Create server
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py"
+command = ("{0} -m ./library/linode_manager"
+           " -a 'name=\"{1}\" api_key=\"{2}\" plan=\"{3}\" datacenter=\"{4}\""
+           " payment_term=\"{5}\" state=\"present\" root_disk_size=\"{6}\""
+           " root_password=\"{7}\" distribution=\"{8}\" swap_disk_size=256 wait=true'").format(
+                test_module, private.LABEL, private.API_KEY, 
+                private.PLAN, private.DATACENTER, private.PAYMENT_TERM, private.DISK_SIZE,
+                private.ROOT_PASS, private.DISTRIBUTION) 
+
+subprocess.call(command, shell=True)
+
+# ===============================================================
+# Ensure that create server isn't run when Linode exists
+# ===============================================================
+command = ("{0} -m ./library/linode_manager"
            " -a 'name=\"{1}\" api_key=\"{2}\" plan=\"{3}\" datacenter=\"{4}\""
            " payment_term=\"{5}\" state=\"present\" root_disk_size=\"{6}\""
            " root_password=\"{7}\" distribution=\"{8}\" swap_disk_size=256 wait=true'").format(
@@ -30,7 +43,7 @@ subprocess.call(command, shell=True)
 # ===============================================================
 # Test reboot module call
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py"
+command = ("{0} -m ./library/linode_manager"
            " -a 'name=\"{1}\" api_key=\"{2}\" state=\"rebooted\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
@@ -39,7 +52,7 @@ subprocess.call(command, shell=True)
 # ===============================================================
 # Test reboot module call with a wait
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py"
+command = ("{0} -m ./library/linode_manager"
            " -a 'name=\"{1}\" api_key=\"{2}\" state=\"rebooted\" wait=\"yes\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
@@ -48,7 +61,7 @@ subprocess.call(command, shell=True)
 # ===============================================================
 # Test shutdown procedure
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py"
+command = ("{0} -m ./library/linode_manager"
            " -a 'name=\"{1}\" api_key=\"{2}\" state=\"shutdown\" wait=\"yes\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
@@ -57,39 +70,26 @@ subprocess.call(command, shell=True)
 # ===============================================================
 # Test boot procedure
 # ===============================================================
-command4 = ("{0} -m ./bin/linode_manager.py"
+command4 = ("{0} -m ./library/linode_manager"
            " -a 'name=\"{1}\" api_key=\"{2}\" state=\"booted\" wait=\"yes\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
 subprocess.call(command, shell=True)
 
 # ===============================================================
-# Ensure that create server isn't run when Linode exists
-# ===============================================================
-command = ("{0} -m ./bin/linode_manager.py"
-           " -a 'name=\"{1}\" api_key=\"{2}\" plan=\"{3}\" datacenter=\"{4}\""
-           " payment_term=\"{5}\" state=\"present\" root_disk_size=\"{6}\""
-           " root_password=\"{7}\" distribution=\"{8}\" swap_disk_size=256 wait=true'").format(
-                test_module, private.LABEL, private.API_KEY, 
-                private.PLAN, private.DATACENTER, private.PAYMENT_TERM, private.DISK_SIZE,
-                private.ROOT_PASS, private.DISTRIBUTION) 
-
-subprocess.call(command, shell=True)
-
-# ===============================================================
 # Tear down server
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py -a "
+command = ("{0} -m ./library/linode_manager -a "
            "'name=\"{1}\" api_key=\"{2}\" state=\"absent\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
-subprocess.call(command, shell=True)
+#subprocess.call(command, shell=True)
 
 # ===============================================================
 # Try to tear down server again. Ensure that changed=false
 # ===============================================================
-command = ("{0} -m ./bin/linode_manager.py -a "
+command = ("{0} -m ./library/linode_manager -a "
            "'name=\"{1}\" api_key=\"{2}\" state=\"absent\"'").format(
                    test_module, private.LABEL, private.API_KEY)
 
-subprocess.call(command, shell=True)
+#subprocess.call(command, shell=True)
